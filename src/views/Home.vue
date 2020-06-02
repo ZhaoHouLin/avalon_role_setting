bf2f92cd81d698970e235787492bb701aa19d218
RaidenLin <rodes5292@gmail.com>
2020-06-02 15:44:06 +0800

    製作首頁淡入動畫

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang='pug'>
+  .home
+    each name in ['merlin','percival','morgana','mordred']
+      div(class=name)
 </template>
 
 <script>
@@ -14,3 +13,38 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+@import '..\assets\style.styl'
+bg(u,p1=0,p2=0,z=cover)
+  background url(u)
+  background-position p1 p2
+  background-size z
+
+.home
+  size(100%,90vh)
+  flexCenter()
+  .merlin
+    size(100%,100%)
+    bg('https://drive.google.com/uc?export=view&id=1mtoXatoI0gWL7KEPyLZ5HW4uTMriYrZH',center,center,)
+    opacity 0
+    animation fadeIn 1s 1s forwards
+  .percival
+    size(100%,100%)
+    bg('https://drive.google.com/uc?export=view&id=1GmDfqdGf_5cvfqYsxqCaWQlgw1C1UpSk',center,center,)
+    opacity 0
+    animation fadeIn 1s 1.5s forwards
+  .morgana
+    size(100%,100%)
+    bg('https://drive.google.com/uc?export=view&id=10U6MH4UWdFN8s09gB8tmRYPMmrkx_R6J',center,center,)
+    opacity 0
+    animation fadeIn 1s 2s forwards
+  .mordred
+    size(100%,100%)
+    bg('https://drive.google.com/uc?export=view&id=15yHtj0llK-C8IhdHGfSgJVgGTLl2t82f',center,center,)
+    opacity 0
+    animation fadeIn 1s 2.5s forwards
+
+
+
+</style>
