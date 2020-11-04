59500e16f0ea4b94688617f002ac0321c507b66c
Raiden Lin <rodes5292@gmail.com>
2020-11-04 17:21:52 +0800

    deploy

---
+++ precache-manifest.d72633155543ecba1157cc53f55e020e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b4577a5ac20baea7e25",
+    "url": "/avalon_role_setting/css/app.235c5993.css"
+  },
+  {
+    "revision": "8b6eb964d73f715fe1680cf082dac131",
+    "url": "/avalon_role_setting/index.html"
+  },
+  {
+    "revision": "ac5083bfa351fae7e4c6",
+    "url": "/avalon_role_setting/js/about.ef6b3223.js"
+  },
+  {
+    "revision": "1b4577a5ac20baea7e25",
+    "url": "/avalon_role_setting/js/app.c4fcfc07.js"
+  },
+  {
+    "revision": "24aec6d5d002e4f7b1fe",
+    "url": "/avalon_role_setting/js/chunk-vendors.5c968262.js"
+  },
+  {
+    "revision": "cee15aef1e865f98959e60f8ddc4e49b",
+    "url": "/avalon_role_setting/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/avalon_role_setting/robots.txt"
+  }
+]);
